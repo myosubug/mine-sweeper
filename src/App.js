753dba9596c86dadf021bdcb0fbb2f5152c081f3
sean <seanoh.dev@gmail.com>
2020-10-11 02:12:34 -0600

    created a board and randomly put mines into the board with initial state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.css';
 import Board from "./components/Board";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>testtt</h1>
-      <Board />
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    mode: "easy",
+    row: 10,
+    col: 10,
+    flag: 10,
+    mine: 10
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>testtt</h1>
+        <Board row={this.state.row} col={this.state.col} mine={this.state.mine}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 
-const Board = () => {
-    return <h1>board</h1>
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            board: this.initialize(props)
+        }
+    }
+    
+    initialize = props => {
+        let board = [];
+        for (let i = 0; i < props.row; i++){
+            board.push([]);
+            for (let j = 0; j < props.col; j++){
+                board[i].push({
+                    x: i,
+                    y: j,
+                    mineCount: 0,
+                    isOpen: false,
+                    hasMine: false,
+                    hasFlag: false
+                });
+            }
+        }
+
+        for (let m = 0; m < props.mine; m++){
+            let rx = Math.floor(Math.random() * props.row);
+            let ry = Math.floor(Math.random() * props.col);
+            let cell = board[rx][ry];
+            if (cell.hasMine){
+                m--;
+            } else {
+                cell.hasMine = true;
+            }
+            console.log(cell);
+        }
+        //for testing board
+        console.table(board);
+    }
+    
+    render(){
+        return <div></div>
+    }
 }
 
 export default Board;
